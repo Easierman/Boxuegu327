01fa18c6b58e68110450807115161591d6a273fb
apple <apple@jd506-24.local>
2018-03-27 16:38:14 +0800

    Initial commit

---
+++ app/src/main/java/boxuegu/android/gdmec/edu/cn/boxuegu/myinfoFragment.java
@@ -0,0 +1,25 @@
+package boxuegu.android.gdmec.edu.cn.boxuegu;
+
+import android.support.v4.app.Fragment;
+import android.view.View;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.os.Bundle;
+
+
+public class myinfoFragment extends Fragment  {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_myinfo, null);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+
+}
